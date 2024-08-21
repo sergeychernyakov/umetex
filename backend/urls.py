45857ad06f9d55be252176ca2cedd304271b68b5
Sergey Chernyakov <chernyakov.sergey@gmail.com>
2024-08-21 10:34:18 +0200

    initial commit

---
+++ backend/urls.py
@@ -0,0 +1,12 @@
+# backend/urls.py
+
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import DocumentViewSet
+
+router = DefaultRouter()
+router.register(r'documents', DocumentViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
